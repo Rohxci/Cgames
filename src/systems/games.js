0f893c5736328c544a7a0fda3cf159dacf9dee91
Rohxci <barchi.ricky.090309@gmail.com>
2026-03-05 17:04:15 +0100

    Create games.js

---
+++ src/systems/games.js
@@ -0,0 +1,17 @@
+const games = {};
+
+module.exports = {
+
+create(channelId, data) {
+games[channelId] = data;
+},
+
+get(channelId) {
+return games[channelId];
+},
+
+delete(channelId) {
+delete games[channelId];
+}
+
+};
