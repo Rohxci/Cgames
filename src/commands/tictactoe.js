91f51eba792559a4895dd272ae81eb2d6a7e7a91
Rohxci <barchi.ricky.090309@gmail.com>
2026-03-05 17:05:19 +0100

    Update tictactoe.js

--- src/commands/tictactoe.js
+++ src/commands/tictactoe.js
@@ -1,6 +1,13 @@
-const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const {
+SlashCommandBuilder,
+ActionRowBuilder,
+ButtonBuilder,
+ButtonStyle
+} = require("discord.js");
+
 const channelCheck = require("../utils/channelCheck");
 const createEmbed = require("../utils/embed");
+const games = require("../systems/games");
 
 module.exports = {
 
@@ -21,42 +28,38 @@ const opponent = interaction.options.getUser("opponent");
 
 if (opponent.bot) {
 return interaction.reply({
-embeds: [createEmbed("❌ Error","You cannot challenge a bot.")]
+embeds:[createEmbed("❌ Error","You cannot challenge a bot.")]
 });
 }
 
 if (opponent.id === interaction.user.id) {
 return interaction.reply({
-embeds: [createEmbed("❌ Error","You cannot challenge yourself.")]
+embeds:[createEmbed("❌ Error","You cannot challenge yourself.")]
 });
 }
 
 const embed = createEmbed(
-"🎮 TicTacToe",
-`${interaction.user} vs ${opponent}\n\nTurn: ${interaction.user}`
+"🎮 TicTacToe Challenge",
+`${interaction.user} challenged ${opponent}`
 );
 
-const row1 = new ActionRowBuilder().addComponents(
-new ButtonBuilder().setCustomId("ttt_0_0").setLabel("⬜").setStyle(ButtonStyle.Secondary),
-new ButtonBuilder().setCustomId("ttt_0_1").setLabel("⬜").setStyle(ButtonStyle.Secondary),
-new ButtonBuilder().setCustomId("ttt_0_2").setLabel("⬜").setStyle(ButtonStyle.Secondary)
-);
+const row = new ActionRowBuilder().addComponents(
 
-const row2 = new ActionRowBuilder().addComponents(
-new ButtonBuilder().setCustomId("ttt_1_0").setLabel("⬜").setStyle(ButtonStyle.Secondary),
-new ButtonBuilder().setCustomId("ttt_1_1").setLabel("⬜").setStyle(ButtonStyle.Secondary),
-new ButtonBuilder().setCustomId("ttt_1_2").setLabel("⬜").setStyle(ButtonStyle.Secondary)
-);
+new ButtonBuilder()
+.setCustomId("ttt_accept")
+.setLabel("Accept")
+.setStyle(ButtonStyle.Success),
 
-const row3 = new ActionRowBuilder().addComponents(
-new ButtonBuilder().setCustomId("ttt_2_0").setLabel("⬜").setStyle(ButtonStyle.Secondary),
-new ButtonBuilder().setCustomId("ttt_2_1").setLabel("⬜").setStyle(ButtonStyle.Secondary),
-new ButtonBuilder().setCustomId("ttt_2_2").setLabel("⬜").setStyle(ButtonStyle.Secondary)
+new ButtonBuilder()
+.setCustomId("ttt_decline")
+.setLabel("Decline")
+.setStyle(ButtonStyle.Danger)
+
 );
 
 await interaction.reply({
-embeds: [embed],
-components: [row1,row2,row3]
+embeds:[embed],
+components:[row]
 });
 
 }
