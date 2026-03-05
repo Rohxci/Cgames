210ca52a3254988fc6431ac78df4762881e8482a
Rohxci <barchi.ricky.090309@gmail.com>
2026-03-05 19:21:34 +0100

    Update channelCheck.js

--- src/utils/channelCheck.js
+++ src/utils/channelCheck.js
@@ -2,19 +2,12 @@ const GAME_CHANNELS = process.env.GAMES_CHANNELS
   ? process.env.GAMES_CHANNELS.split(",")
   : [];
 
-module.exports = async function channelCheck(interaction) {
+module.exports = function channelCheck(interaction) {
 
 if (GAME_CHANNELS.length === 0) return true;
 
 if (!GAME_CHANNELS.includes(interaction.channelId)) {
-
-await interaction.reply({
-content: `🎮 Please use game commands in a games channel.`,
-ephemeral: true
-});
-
 return false;
-
 }
 
 return true;
