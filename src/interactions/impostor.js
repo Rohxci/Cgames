b4a9ed2a7c1e307dd7bc0b9abc18b317c4fa4fed
Rohxci <barchi.ricky.090309@gmail.com>
2026-03-05 18:45:29 +0100

    Update impostor.js

--- src/interactions/impostor.js
+++ src/interactions/impostor.js
@@ -8,7 +8,7 @@ StringSelectMenuBuilder
 const games = require("../systems/games");
 
 const MAX_PLAYERS = 10;
-const MIN_PLAYERS = 3;
+const MIN_PLAYERS = 2;
 
 function pickRandom(arr){
 return arr[Math.floor(Math.random()*arr.length)];
