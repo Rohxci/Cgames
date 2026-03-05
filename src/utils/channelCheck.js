f8e7e6dddd4956f5e6df9c8abc11ea9ff5cfd8dd
Rohxci <barchi.ricky.090309@gmail.com>
2026-03-05 19:26:04 +0100

    Update channelCheck.js

--- src/utils/channelCheck.js
+++ src/utils/channelCheck.js
@@ -1,10 +1,12 @@
-const GAME_CHANNELS = process.env.GAMES_CHANNELS
-  ? process.env.GAMES_CHANNELS.split(",")
-  : [];
+const GAME_CHANNELS = [
+"1479124545117097984",
+"1479180040842252288",
+"1479180070546309250",
+"1479180141778309130",
+"1479180177597530275"
+];
 
 module.exports = function channelCheck(interaction) {
-
-if (GAME_CHANNELS.length === 0) return true;
 
 if (!GAME_CHANNELS.includes(interaction.channelId)) {
 return false;
