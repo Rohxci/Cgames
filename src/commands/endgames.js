59bbabf24b5ef2874a4e0e6f9ced22efcd50069e
Rohxci <barchi.ricky.090309@gmail.com>
2026-03-06 17:47:00 +0100

    Update endgames.js

--- src/commands/endgames.js
+++ src/commands/endgames.js
@@ -5,6 +5,18 @@ PermissionFlagsBits
 
 const games = require("../systems/games");
 
+/* channels where games can exist */
+
+const GAME_CHANNELS = [
+"1479124545117097984",
+"1479180040842252288",
+"1479180070546309250",
+"1479180141778309130",
+"1479180177597530275",
+"1479481732892987433",
+"1479481762064105552"
+];
+
 module.exports = {
 
 data: new SlashCommandBuilder()
@@ -14,9 +26,31 @@ data: new SlashCommandBuilder()
 
 async execute(interaction){
 
-const activeGames = Object.keys(games._games || {});
+let ended = 0;
 
-if(activeGames.length === 0){
+for(const id of GAME_CHANNELS){
+
+const game = games.get(id);
+
+if(!game) continue;
+
+try{
+
+const channel = await interaction.client.channels.fetch(id);
+
+if(channel){
+await channel.send("🛑 A staff member ended the game.");
+}
+
+}catch{}
+
+games.delete(id);
+
+ended++;
+
+}
+
+if(ended === 0){
 
 return interaction.reply({
 content:"No active games.",
@@ -25,26 +59,8 @@ ephemeral:true
 
 }
 
-for(const channelId of activeGames){
-
-try{
-
-const channel = await interaction.client.channels.fetch(channelId);
-
-if(channel){
-
-channel.send("🛑 A staff member ended the game.");
-
-}
-
-}catch{}
-
-games.delete(channelId);
-
-}
-
 await interaction.reply({
-content:`🛑 ${activeGames.length} game(s) terminated.`,
+content:`🛑 ${ended} game(s) terminated.`,
 ephemeral:true
 });
 
